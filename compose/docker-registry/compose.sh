3850960f065260ce9543c04b3677a4ed1ebb685b
George Papaefthymiou <george.yord@gmail.com>
2015-06-05 10:07:39 +0300

    Create ssh keys for private registry

--- compose/docker-registry/compose.sh
+++ compose/docker-registry/compose.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+FILE=./data/keys/ssl.pem
+
+if [ -f $FILE ]; then
+   echo "SSL pem file exists, continuing..."
+else
+   echo "SSL pem file not found, we will generate one..."
+   openssl genrsa 2048 > ssl.key
+   openssl req -new -x509 -nodes -sha1 -days 3650 -key ssl.key > ssl.pem
+fi
+
 docker-compose $*
 
 if [[ $(uname) == "Linux" ]]; then
