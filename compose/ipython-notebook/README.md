31dd2a0fe9c45f9d3718ddcab9482587b53b63a8
George Yord <george.yord@gmail.com>
2016-03-09 04:27:33 -0500

    Docker compose version 2 - total refactoring

--- compose/ipython-notebook/README.md
+++
@@ -1,4 +0,0 @@
-Docker iPython Notebook container
----------------------------------
-
-Use this container to run iPython Notebook on a mounted directory 
--- compose/kibana-fluentd-remote/bin/kibana/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# elasticsearch start
-/usr/share/elasticsearch/bin/elasticsearch -d
-
-# kibana start
-/usr/sbin/nginx -c /etc/nginx/nginx.conf
-
-td-agent
