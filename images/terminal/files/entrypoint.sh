b233ec53a4267051162004abe4bca7a4b61ef486
George Yord <george.yord@gmail.com>
2021-04-12 17:35:33 +0300

    Introduce terminal docker image

--- images/terminal/files/entrypoint.sh
+++ images/terminal/files/entrypoint.sh
@@ -37,18 +37,6 @@ if [[ "${UID:-}" != "" ]]; then
             --groups "sudo,docker" \
             "${USERNAME}"
     id "${USERNAME}" -nG
-
-    # exec /usr/sbin/gosu \
-    # "${USERNAME}:${USERNAME}" \
-    # sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v${ZSH_IN_DOCKER_VERSION}/zsh-in-docker.sh)" -- \
-    #   -t https://github.com/denysdovhan/spaceship-prompt \
-    #   -a 'SPACESHIP_PROMPT_ADD_NEWLINE="false"' \
-    #   -a 'SPACESHIP_PROMPT_SEPARATE_LINE="false"' \
-    #   -p https://github.com/zsh-users/zsh-autosuggestions \
-    #   -p https://github.com/zsh-users/zsh-completions \
-    #   -p https://github.com/zsh-users/zsh-history-substring-search \
-    #   -p https://github.com/zsh-users/zsh-syntax-highlighting \
-    #   -p 'history-substring-search'
   fi
 else
   USERNAME="$(whoami)"
